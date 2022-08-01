4867a8b263f105deb11b14cc89b3b36485f5b92b
mhmmdsyahputra <putra.cokers@gmail.com>
2022-08-01 21:58:01 +0700

    fix time3

--- src/views/Group.jsx
+++ src/views/Group.jsx
@@ -44,7 +44,10 @@ const Group = () => {
   const sendmessege = () => {
     const db = getDatabase();
     const now = new Date()
-    const time = String(now.getHours() + ':' + now.getMinutes()).padStart(5, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const time = hours + ":" + minutes
+    // const time = String(now.getHours() + ':' + now.getMinutes()).padStart(5, '0');
     push(ref(db, 'groups/' + id + '/chat/'), {
       uid: uid,
       name: inputname,
